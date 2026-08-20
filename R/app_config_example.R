2ca89d50a0df4acbf487d86a5121aa0d6bd530cc
Alex F. Bokov, Ph.D. <alex.bokov@gmail.com>
2026-08-19 23:57:34 -0500

    Document topic order as curriculum priority

--- R/app_config_example.R
+++ R/app_config_example.R
@@ -3,6 +3,8 @@ APP_CONFIG <- list(
   week_id = "week-01",
   app_name = "r-syntax-drills",
   questions_per_week = 10L,
+  # Transitional PR14 name: order these from earliest to most advanced topic.
+  # PR15 will rename unlocked_topics to reflect its curriculum-priority meaning.
   unlocked_topics = c(
     "vector_creation",
     "vector_indexing"
